6676910337c13d38c65e6ea64aa31a475e09192e
Rahimullah42 <geeky.rahim@gmail.com>
2025-02-18 15:54:21 +0500

    windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,211 +1,181 @@
-import 'dart:io';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:openvpn_flutter/openvpn_flutter.dart';
+import 'package:wireguard_flutter/wireguard_flutter.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('WireGuard Example App'),
+        ),
+        body: const MyApp(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: VPNPage(),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class VPNPage extends StatefulWidget {
-  @override
-  _VPNPageState createState() => _VPNPageState();
-}
+class _MyAppState extends State<MyApp> {
+  final wireguard = WireGuardFlutter.instance;
 
-class _VPNPageState extends State<VPNPage> {
-  final OpenVPN openvpn = OpenVPN();
-  VpnStatus? currentState;
-   String isConnected = "";
-  late OpenVPN engine;
-//VpnStatus? vpnStatus;
-  String? stage;
-  bool granted = false;
-
-  // Replace these with your VPN configuration
-  final String config = '''
-  client
-  dev tun
-  proto udp
-  remote 3.113.44.172 443  # Replace with your server's IP address
-  resolv-retry infinite
-  nobind
-  persist-key
-  persist-tun
-  remote-cert-tls server
-  cipher AES-256-CBC
-  verb 3
-
-  <ca>
-  -----BEGIN CERTIFICATE-----
-  MIIBwzCCAWigAwIBAgIUdhuVqzDRz9FWfGiBnRQdIGiSxNcwCgYIKoZIzj0EAwIw
-  FzEVMBMGA1UEAwwMQ2lwaGVyLUhhdmVuMB4XDTI0MTExOTA2MjEwMVoXDTM0MTEx
-  NzA2MjEwMVowFzEVMBMGA1UEAwwMQ2lwaGVyLUhhdmVuMFkwEwYHKoZIzj0CAQYI
-  KoZIzj0DAQcDQgAERW0KgJXE+gwvx6+nnGWyG2Dxa1K79YnTu7HJr5bhpFYu5IRB
-  MX+Zv3wKhfzT9JyvF49hU0nJ19NabigELPVIgKOBkTCBjjAMBgNVHRMEBTADAQH/
-  MB0GA1UdDgQWBBRROOhJ/fdoGsg6E3juIIBryy+FTjBSBgNVHSMESzBJgBRROOhJ
-  /fdoGsg6E3juIIBryy+FTqEbpBkwFzEVMBMGA1UEAwwMQ2lwaGVyLUhhdmVughR2
-  G5WrMNHP0VZ8aIGdFB0gaJLE1zALBgNVHQ8EBAMCAQYwCgYIKoZIzj0EAwIDSQAw
-  RgIhAJiq/8fS7lh/xkSg6h1gekt/L1Ie2RJu09KY96/jVB36AiEAoqe5Wu23IYj4
-  qJJj91yiB74yDvfblw4UrS/YsUsN0I8=
-  -----END CERTIFICATE-----
-  </ca>
-  <cert>
-  -----BEGIN CERTIFICATE-----
-  MIIByTCCAW+gAwIBAgIQBiLggSvmakscc6MfdD3LcDAKBggqhkjOPQQDAjAXMRUw
-  EwYDVQQDDAxDaXBoZXItSGF2ZW4wHhcNMjQxMjAyMDY1MzI1WhcNMjcwMzA3MDY1
-  MzI1WjAQMQ4wDAYDVQQDDAVyYWhpbTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IA
-  BCWBL5XVOsY4SOQpESbbceWhKmPjwJXzdZxLXMnG+lvUDG5c+co1rgI/kE3Jx99I
-  LSafubUTtDxd5vJ6k2mFLpejgaMwgaAwCQYDVR0TBAIwADAdBgNVHQ4EFgQUM2Od
-  zNclGtDD79Fhnpl3rJnEV9EwUgYDVR0jBEswSYAUUTjoSf33aBrIOhN47iCAa8sv
-  hU6hG6QZMBcxFTATBgNVBAMMDENpcGhlci1IYXZlboIUdhuVqzDRz9FWfGiBnRQd
-  IGiSxNcwEwYDVR0lBAwwCgYIKwYBBQUHAwIwCwYDVR0PBAQDAgeAMAoGCCqGSM49
-  BAMCA0gAMEUCIQCkKS1mcZBiJJdJNPfJIUEzuDviAh1SNjI1OStuuqiR9wIgVsXZ
-  7nr8ycVuijQKZ4m5J4z90O1posGNEm/GST4Pq8s=
-  -----END CERTIFICATE-----
-  </cert>
-  <key>
-  -----BEGIN PRIVATE KEY-----
-  MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgIytCYtqfMholraD8
-  hs1cLd8leqEtxSOl8jv+CvYWGYChRANCAAQlgS+V1TrGOEjkKREm23HloSpj48CV
-  83WcS1zJxvpb1AxuXPnKNa4CP5BNycffSC0mn7m1E7Q8XebyepNphS6X
-  -----END PRIVATE KEY-----
-  </key>
-  <tls-crypt>
-  #
-  # 2048 bit OpenVPN static key
-  #
-  -----BEGIN OpenVPN Static key V1-----
-  584cff85f0db365d3e49a659522bd9f2
-  3e7f35fa96ecf65d9c65de797f26af4f
-  42a3a37fc04f404e43a37a4b68066f41
-  c947126cf10704bdfe4ba6be1f0bc9dd
-  6972076d8c48ea6b9ca3c321118e1211
-  07c149d3ef9267048f1799c96a01570a
-  291fa05e08b4887e75755c7f759066c9
-  3fe7e811e6656ac1ee7c713a1b736bb8
-  d06079866c59072df46404e28cdfbe5f
-  8d282457c1af65dd4efb00f1d6cfdb31
-  1518531056dfde332a672a08e3c290ac
-  9b25e5f0cefbe99cb20bd70669eb838c
-  89e8ce9aabd07b2acfccb2d9c87929d8
-  2b9ceeb6b16ec56be6195fbe254d38ba
-  c8e7d3391a6afb7a49cf06c4a4bfbd6e
-  9644940cbaf474f323aa287f1d4dce7f
-  -----END OpenVPN Static key V1-----
-
-  </tls-crypt>
-    ''';
-
-  final String username = '';
-  final String password = '';
+  late String name;
 
   @override
   void initState() {
     super.initState();
-    initOpenVpn();
+    wireguard.vpnStageSnapshot.listen((event) {
+      debugPrint("status changed $event");
+      if (mounted) {
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('status changed: $event'),
+        ));
+      }
+    });
+    name = 'my_wg_vpn';
   }
 
-
-  void initOpenVpn(){
-    engine = OpenVPN(
-      onVpnStatusChanged: (data) {
-      },
-      onVpnStageChanged: (data, raw) {
-        isConnected = raw;
-        stage = raw;
-      },
-    );
-
-    engine.initialize(
-      groupIdentifier: "group.net.cipherhaven.vpn.main",
-      providerBundleIdentifier:
-      "net.cipherhaven.vpn.main.VPNExtension",
-      localizedDescription: "Test VPN",
-      lastStage: (stage) {
-        this.stage = stage.name;
-      },
-      lastStatus: (status) {
-        // vpnStatus = status;
-      },
-    );
-  }
-
-
-  Future<void> connectToVpn() async {
-    // Ensure Android permission is granted
-    if (Platform.isAndroid && !granted) {
-      bool permissionGranted = await engine.requestPermissionAndroid();
-      granted = permissionGranted;
-      if (!permissionGranted) return; // Exit if permission not granted
+  Future<void> initialize() async {
+    try {
+      await wireguard.initialize(interfaceName: name);
+      debugPrint("initialize success $name");
+    } catch (error, stack) {
+      debugPrint("failed to initialize: $error\n$stack");
     }
-    await engine.connect(
-      config,
-      "Test VPN",
-      username: username,
-      password: password,
-      certIsRequired: true,
-    );
   }
 
-  Future<void> disconnectVpn() async {
-    engine.disconnect();
+  void startVpn() async {
+    try {
+      await wireguard.startVpn(
+        serverAddress: '167.235.55.239:51820',
+        wgQuickConfig: conf,
+        providerBundleIdentifier: 'com.example.vpnIosTest.WGExtension',
+      );
+    } catch (error, stack) {
+      debugPrint("failed to start $error\n$stack");
+    }
   }
 
+  void disconnect() async {
+    try {
+      await wireguard.stopVpn();
+    } catch (e, str) {
+      debugPrint('Failed to disconnect $e\n$str');
+    }
+  }
+
+  void getStatus() async {
+    debugPrint("getting stage");
+    final stage = await wireguard.stage();
+    debugPrint("stage: $stage");
+
+    if (mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('stage: $stage'),
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('VPN Client'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'VPN Status: ${currentState?.toString() ?? 'Not connected'}',
-              style: TextStyle(fontSize: 20),
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 20),
+          TextButton(
+            onPressed: initialize,
+            style: ButtonStyle(
+                minimumSize:
+                MaterialStateProperty.all<Size>(const Size(100, 50)),
+                padding: MaterialStateProperty.all(
+                    const EdgeInsets.fromLTRB(20, 15, 20, 15)),
+                backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.blueAccent),
+                overlayColor: MaterialStateProperty.all<Color>(
+                    Colors.white.withOpacity(0.1))),
+            child: const Text(
+              'initialize',
+              style: TextStyle(color: Colors.white),
             ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-              ),
-              onPressed: (){
-                isConnected != "connected" ? null : connectToVpn();
-              },
-              child: Text(
-                'CONNECT',
-                style: TextStyle(fontSize: 20, color: Colors.white),
-              ),
+          ),
+          const SizedBox(height: 20),
+          TextButton(
+            onPressed: startVpn,
+            style: ButtonStyle(
+                minimumSize:
+                MaterialStateProperty.all<Size>(const Size(100, 50)),
+                padding: MaterialStateProperty.all(
+                    const EdgeInsets.fromLTRB(20, 15, 20, 15)),
+                backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.blueAccent),
+                overlayColor: MaterialStateProperty.all<Color>(
+                    Colors.white.withOpacity(0.1))),
+            child: const Text(
+              'Connect',
+              style: TextStyle(color: Colors.white),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-              ),
-              onPressed:(){
-                isConnected == "connected" ? disconnectVpn() : null;
-              },
-              child: Text(
-                'DISCONNECT',
-                style: TextStyle(fontSize: 20, color: Colors.white),
-              ),
+          ),
+          const SizedBox(height: 20),
+          TextButton(
+            onPressed: disconnect,
+            style: ButtonStyle(
+                minimumSize:
+                MaterialStateProperty.all<Size>(const Size(100, 50)),
+                padding: MaterialStateProperty.all(
+                    const EdgeInsets.fromLTRB(20, 15, 20, 15)),
+                backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.blueAccent),
+                overlayColor: MaterialStateProperty.all<Color>(
+                    Colors.white.withOpacity(0.1))),
+            child: const Text(
+              'Disconnect',
+              style: TextStyle(color: Colors.white),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 20),
+          TextButton(
+            onPressed: getStatus,
+            style: ButtonStyle(
+                minimumSize:
+                MaterialStateProperty.all<Size>(const Size(100, 50)),
+                padding: MaterialStateProperty.all(
+                    const EdgeInsets.fromLTRB(20, 15, 20, 15)),
+                backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.blueAccent),
+                overlayColor: MaterialStateProperty.all<Color>(
+                    Colors.white.withOpacity(0.1))),
+            child: const Text(
+              'Get status',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+const String conf = '''[Interface]
+PrivateKey = 0IZmHsxiNQ54TsUs0EQ71JNsa5f70zVf1LmDvON1CXc=
+Address = 10.8.0.4/32
+DNS = 1.1.1.1
+
+
+[Peer]
+PublicKey = 6uZg6T0J1bHuEmdqPx8OmxQ2ebBJ8TnVpnCdV8jHliQ=
+PresharedKey = As6JiXcYcqwjSHxSOrmQT13uGVlBG90uXZWmtaezZVs=
+AllowedIPs = 0.0.0.0/0, ::/0
+PersistentKeepalive = 0
+Endpoint = 38.180.13.85:51820''';
